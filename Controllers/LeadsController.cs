d5c219b69a252a97ea108cbd8a7c03107cc244ba
sharath-98 <sharathchander.p@gmail.com>
2023-05-18 16:13:04 -0500

    Authorize page view only when logged in

--- Controllers/LeadsController.cs
+++ Controllers/LeadsController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LoginApp.Data;
 using LoginApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LoginApp.Controllers
 {
+    [Authorize]
     public class LeadsController : Controller
     {
         private readonly ApplicationDbContext _context;
