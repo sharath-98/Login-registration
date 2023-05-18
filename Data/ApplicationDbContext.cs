ddb0f509d2ff67990cc3fef17283146c4f4fe56f
sharath-98 <sharathchander.p@gmail.com>
2023-05-18 14:45:49 -0500

    DB injection setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LoginApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        DbSet<SalesLeadEntity> SalesLead { get; set; }
+    }
+}
